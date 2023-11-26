08ae3f0597f233b94461e2e3f4ae19212c1f450b
KSJ <gga04002@gmail.com>
2023-11-26 17:50:00 +0900

    first commit

---
+++ src/main/java/com/example/mub/MubProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.mub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MubProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MubProjectApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/mub/controller/TestController.java
@@ -0,0 +1,7 @@
+package com.example.mub.controller;
+
+public class TestController {
+
+
+
+}
