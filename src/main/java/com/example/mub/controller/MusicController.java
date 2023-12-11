519def597f10378a589048928d903084241e8d60
KSJ <gga04002@gmail.com>
2023-12-11 09:24:30 +0900

    기타 수정

---
+++ src/main/java/com/example/mub/controller/MusicController.java
@@ -0,0 +1,19 @@
+package com.example.mub.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Controller
+@RequestMapping("music")
+public class MusicController {
+
+	@GetMapping("music")
+    public String music() {
+
+        return "music/music";
+    }
+}
