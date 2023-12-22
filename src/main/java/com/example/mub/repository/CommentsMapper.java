11ffbfe7ed6582fb9a2db2a74198ccd45bd1df10
KyuHunPARK <qkrrbgns@gmail.com>
2023-12-22 16:28:11 +0900

    댓글기능구현

--- src/main/java/com/example/mub/repository/CommentsMapper.java
+++ src/main/java/com/example/mub/repository/CommentsMapper.java
@@ -1,5 +1,21 @@
 package com.example.mub.repository;
 
-public interface CommentsMapper {
+import java.util.List;
 
+import org.apache.ibatis.annotations.Mapper;
+
+import com.example.mub.model.comments.Comments;
+
+@Mapper
+public interface CommentsMapper {
+	//리플등록
+	void saveComments(Comments comments);
+	//리플읽기
+	Comments findComments(Long comments_id);
+	//리플목록
+	List<Comments> findAllComments(Long comments_board);
+	//리플수정
+	void updateComments(Comments comments);
+	//리플삭제
+	void removeComments(Long comments_board);
 }
